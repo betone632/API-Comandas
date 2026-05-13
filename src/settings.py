dfa01e0c15f870ce5f77b0007d41155637f03d90
betone632 <betone632@hotmail.com>
2026-05-13 19:05:50 -0300

    Docker image 1

--- src/settings.py
+++ src/settings.py
@@ -22,9 +22,9 @@ DB_PASS = os.getenv("DB_PASS")
 # Ajusta STR_DATABASE conforme gerenciador escolhido
 if DB_SGDB == 'sqlite': # SQLite
     STR_DATABASE = f"sqlite:///{DB_NAME}.db"
-#elif DB_SGDB == 'mysql': # MySQL
-    #import pymysql
-    #STR_DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
+elif DB_SGDB == 'mysql': # MySQL
+    import pymysql
+    STR_DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
 #elif DB_SGDB == 'mssql': # SQL Server
     #import pymssql
     #STR_DATABASE = f"mssql+pymssql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8"
